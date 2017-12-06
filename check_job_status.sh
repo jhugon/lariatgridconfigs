9f74a7c877e10b8734b10949c4e62e34b8e9f3ae
Justin Hugon <5174443+jhugon@users.noreply.github.com>
2017-12-06 11:16:50 -0600

    added data_run8603_beam.xml; other version renamed cosmics
      cosmics version trying to scale up files/job by 10 to 50
      check_job_status.py now does events/ job

--- check_job_status.sh
+++ check_job_status.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-usagestr="usage: ./check_job_status.sh [-v <0/1>] <dirname> <njobs>"
+usagestr="usage: ./check_job_status.sh [-v <0/1>] [-e <0/1>] <dirname> <njobs>"
 
 verbose=""
+eventsverbose=""
+eventspass=""
+eventsall=""
 
-while getopts "v:" opt; do
+while getopts "v:e:pa" opt; do
   case $opt in
     v)
       verbose=$OPTARG
-      echo $verbose
+      ;;
+    e)
+      eventsverbose=$OPTARG
+      echo "option e!"
+      ;;
+    p)
+      eventspass="1"
+      echo "option p!"
+      ;;
+    a)
+      eventsall="1"
+      echo "option a!"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -36,8 +50,20 @@ for i in $(seq 1 $njobs); do
     stage0stat=$(cat $d/larStage0.stat 2>/dev/null)
     stage1stat=$(cat $d/larStage1.stat 2>/dev/null)
     host=$(cat $d/hostname.txt 2>/dev/null)
+    eventsstr=""
+    if [ -n "$eventsverbose" ]; then
+      eventsstr=$(grep -o "Events total.*" $d/larStage"$eventsverbose".out 2>/dev/null)
+    elif [ -n "$eventspass" ]; then
+      eventsstr0=$(grep -o 'Events total = [0-9]* passed = [0-9]*' $d/larStage0.out 2>/dev/null | cut -f3- -d"=")
+      eventsstr1=$(grep -o 'Events total = [0-9]* passed = [0-9]*' $d/larStage1.out 2>/dev/null | cut -f3- -d"=")
+      eventsstr=$eventsstr0$eventsstr1
+    elif [ -n "$eventsall" ]; then
+      eventsstr0=$(grep -o 'Events total = [0-9]*' $d/larStage0.out 2>/dev/null | cut -f2- -d"=")
+      eventsstr1=$(grep -o 'Events total = [0-9]*' $d/larStage1.out 2>/dev/null | cut -f2- -d"=")
+      eventsstr=$eventsstr0$eventsstr1
+    fi
     #echo $i $jobid $(cat $d/larStage0.stat) $(cat $d/larStage1.stat) $(cat $d/hostname.txt) $stage0time $stage1time; 
-    echo $i $jobid $stage0stat $stage1stat $host $stage0time $stage1time; 
+    echo $i $jobid $stage0stat $stage1stat $host $stage0time $stage1time $eventsstr
     if [ -n "$verbose" ]; then
       tail -n 20 $d/larStage"$verbose".out
     fi
